22b5c4d25c38415bb38339176fee34d4e66b997d
ricardgardellagarcia <83lolot9y>
2019-06-24 23:50:05 +0200

    Finish the code. The analysis still have to be done.

--- Graphs/DataAnalysis.py
+++ Graphs/DataAnalysis.py
@@ -24,8 +24,62 @@ plt.figure(figsize=(10,7)) # 10 is width, 7 is height
 plt.plot(data['Horizon'].unique(),dataArima['NRMSE'].loc[dataArima['Scenario'] == "Nor"], 'c', label='ARIMA')
 plt.plot(data['Horizon'].unique(),dataRZ['NRMSE'].loc[dataRZ['Scenario'] == "Nor"], 'm', label='RZ')
 plt.plot(data['Horizon'].unique(), dataNN['NRMSE'].loc[dataNN['Scenario'] == "Nor"], 'y', label='NN')
-plt.title('Prediction Horizon in the Congested Scenario')
+plt.title('Prediction Horizon in the Normal Scenario')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.legend(loc='best')
 plt.show()
+
+
+#AGGREGATION ANALYSIS
+data = pd.read_csv("PlotData/aggregation.csv")
+dataArima = data.loc[data['Algorithm'] == 'ARIMA']
+dataRZ = data.loc[data['Algorithm'] == 'RZ']
+dataNN = data.loc[data['Algorithm'] == 'NN']
+#aggregation congested
+plt.figure(figsize=(10,7)) # 10 is width, 7 is height
+plt.plot(data['aggregation'].unique(),dataArima['NRMSE'].loc[dataArima['Scenario'] == "Con"], 'c', label='ARIMA')
+plt.plot(data['aggregation'].unique(),dataRZ['NRMSE'].loc[dataRZ['Scenario'] == "Con"], 'm', label='RZ')
+plt.plot(data['aggregation'].unique(), dataNN['NRMSE'].loc[dataNN['Scenario'] == "Con"], 'y', label='NN')
+plt.title('Aggregation in the Congested Scenario')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend(loc='best')
+plt.show()
+#aggregation normal
+plt.figure(figsize=(10,7)) # 10 is width, 7 is height
+plt.plot(data['aggregation'].unique(),dataArima['NRMSE'].loc[dataArima['Scenario'] == "Nor"], 'c', label='ARIMA')
+plt.plot(data['aggregation'].unique(),dataRZ['NRMSE'].loc[dataRZ['Scenario'] == "Nor"], 'm', label='RZ')
+plt.plot(data['aggregation'].unique(), dataNN['NRMSE'].loc[dataNN['Scenario'] == "Nor"], 'y', label='NN')
+plt.title('Aggregation in the Normal Scenario')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend(loc='best')
+plt.show()
+
+#AMOUNT OF DATA ANALYSIS
+data = pd.read_csv("PlotData/amount.csv")
+dataArima = data.loc[data['Algorithm'] == 'ARIMA']
+dataRZ = data.loc[data['Algorithm'] == 'RZ']
+dataNN = data.loc[data['Algorithm'] == 'NN']
+#aggregation congested
+plt.figure(figsize=(10,7)) # 10 is width, 7 is height
+plt.plot(data['Amount'].unique(),dataArima['NRMSE'].loc[dataArima['Scenario'] == "Con"], 'c', label='ARIMA')
+plt.plot(data['Amount'].unique(),dataRZ['NRMSE'].loc[dataRZ['Scenario'] == "Con"], 'm', label='RZ')
+plt.plot(data['Amount'].unique(), dataNN['NRMSE'].loc[dataNN['Scenario'] == "Con"], 'y', label='NN')
+plt.title('Amount in the Congested Scenario')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend(loc='best')
+plt.show()
+#aggregation normal
+plt.figure(figsize=(10,7)) # 10 is width, 7 is height
+plt.plot(data['Amount'].unique(),dataArima['NRMSE'].loc[dataArima['Scenario'] == "Nor"], 'c', label='ARIMA')
+plt.plot(data['Amount'].unique(),dataRZ['NRMSE'].loc[dataRZ['Scenario'] == "Nor"], 'm', label='RZ')
+plt.plot(data['Amount'].unique(), dataNN['NRMSE'].loc[dataNN['Scenario'] == "Nor"], 'y', label='NN')
+plt.title('Amount in the Normal Scenario')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend(loc='best')
+plt.show()
+
